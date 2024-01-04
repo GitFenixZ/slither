450ec37e26254b7ac32035b6d1196116f5abd3c0
GUETTEVILLE nathan <nathan.guetteville@etu.u-paris.fr>
2024-01-04 01:27:45 +0100

    feat(IA): computer snake is now confined to the grid

--- src/main/java/controller/ComputerController.java
+++ src/main/java/controller/ComputerController.java
@@ -5,31 +5,57 @@ import model.Direction;
 import model.GridModel;
 import view.GridView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class ComputerController {
 
     public static void initComputerController(GridModel model, GridView view, GridController controller) {
-        Random rand = new Random();
-
         view.getScene().addEventHandler(KeyEvent.KEY_PRESSED, (event) -> {
             switch (event.getCode()) {
                 case UP:
                 case DOWN:
                 case LEFT:
                 case RIGHT:
-                    int randNum = rand.nextInt(3);
-                    Direction randDir = switch (randNum) {
-                        case 0 -> Direction.UP;
-                        case 1 -> Direction.DOWN;
-                        case 2 -> Direction.LEFT;
-                        default -> Direction.RIGHT;
-                    };
-                    controller.movePlayer(model.getComputer(), randDir);
+                    Direction randomDirection = getRandomDirection(model);
+                    if (randomDirection != null) {
+                        controller.movePlayer(model.getComputer(), randomDirection);
+                    }
                     break;
                 default:
                     break;
             }
         });
     }
+
+    private static Direction getRandomDirection(GridModel model) {
+        List<Direction> possibleDirections = getPossibleDirections(model);
+        int nbOfPossibleDirections = possibleDirections.size();
+        if (nbOfPossibleDirections != 0) {
+            Random rand = new Random();
+            return possibleDirections.get(rand.nextInt(nbOfPossibleDirections));
+        }
+        return null;
+    }
+
+    private static List<Direction> getPossibleDirections(GridModel model) {
+        int currentComputerPositionX = model.getComputer().getSnake().getHeadX();
+        int currentComputerPositionY = model.getComputer().getSnake().getHeadY();
+        List<Direction> possibleDirections = new ArrayList<>();
+        if (currentComputerPositionY != 0) {
+            possibleDirections.add(Direction.UP);
+        }
+        if (currentComputerPositionY != model.getHeight() - 1) {
+            possibleDirections.add(Direction.DOWN);
+        }
+        if (currentComputerPositionX != 0) {
+            possibleDirections.add(Direction.LEFT);
+        }
+        if (currentComputerPositionX != model.getWidth() - 1) {
+            possibleDirections.add(Direction.RIGHT);
+        }
+        return possibleDirections;
+    }
+
 }
